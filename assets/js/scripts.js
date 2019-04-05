fcd5545a00702e386147903f0d2dd91a5d6a5ab3
aevers3 <aevers3@users.noreply.github.com>
2019-04-05 16:33:12 -0400

    CSS, chart color updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,20 +7,20 @@ var myChart = new Chart(ctx, {
             label: '# of Votes',
             data: [10, 15, 12, 11, 12, 1],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
+                'rgba(226, 76, 38, 0.2)',
+                'rgba(86, 60, 190, 0.3)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(241, 224, 90, 0.2)',
+                'rgba(31, 255, 93, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
+                'rgba(226, 76, 38, 1)',
+                'rgba(86, 60, 190, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 206, 86, 1)',
+                'rgba(241, 224, 90, 1)',
+                'rgba(31, 255, 93, 1)'
             ],
             borderWidth: 1
         }]
@@ -32,10 +32,10 @@ var myChart = new Chart(ctx, {
                     var label = data.labels[tooltipItem.index];
                     return label;
                 },
-                    scales: {
-                        
-                    },
-                }
+                scales: {
+
+                },
             }
         }
-    });
+    }
+});
